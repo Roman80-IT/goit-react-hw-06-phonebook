764851fd0e8353f9e7780597297c1b2a0542dd49
Roman <jrj80mail@gmail.com>
2023-11-05 18:52:51 +0200

    +Contact +ContactList

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,31 @@
 import { FaTimes } from 'react-icons/fa';
 import { Button, Item } from './Contact.styled';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'components/redux/contactsOperationReducer';
 
-export const Contact = ({ id, name, number, onDelete }) => {
+// export const Contact = ({ id, name, number, onDelete }) => {
+//   return (
+//     <Item>
+//       {name}: {number}
+//       <Button
+//         type="button"
+//         onClick={() => onDelete(id)}
+//         title={`Delete ${name}`}
+//       >
+//         <FaTimes />
+//       </Button>
+//     </Item>
+//   );
+// };
+
+export const Contact = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteContact(id));
+
   return (
     <Item>
       {name}: {number}
-      <Button
-        type="button"
-        onClick={() => onDelete(id)}
-        title={`Delete ${name}`}
-      >
+      <Button type="button" onClick={handleDelete} title={`Delete ${name}`}>
         <FaTimes />
       </Button>
     </Item>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,43 @@
+import { useSelector } from 'react-redux';
 import { Contact } from 'components/Contact/Contact';
+import { getContacts, getFilter } from 'components/redux/selectors';
 
-export const ContactList = ({ contacts, onDelete }) => {
+// export const ContactList = ({ contacts, onDelete }) => {
+//   return (
+//     <ul>
+//       {contacts.map(contact => (
+//         <li key={contact.id}>
+//           <Contact
+//             id={contact.id}
+//             name={contact.name}
+//             number={contact.number}
+//             onDelete={onDelete}
+//           />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+const getFilteredContacts = (contacts, filter) => {
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+};
+
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const filteredContacts = getFilteredContacts(contacts, filter);
+
   return (
     <ul>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <li key={contact.id}>
           <Contact
             id={contact.id}
             name={contact.name}
             number={contact.number}
-            onDelete={onDelete}
           />
         </li>
       ))}
